14815f3db1367f54fe992060561e1bdcde76f998
Jerzayl <62535706+jerza99@users.noreply.github.com>
2024-05-09 15:20:16 -0600

    actualizacion

--- config/db.php
+++ config/db.php
@@ -5,8 +5,16 @@
     class Database{
 
         // Metodo estatico para establecer una conexión con una base de datos MySQL y configurar el conjunto de caracteres de la conexión.
+
         public static function connect(){
-            $db = new mysqli('localhost', 'root', '', 'tienda');
+
+            // Variables
+            $host = 'localhost';
+            $username = 'root';
+            $password = '';
+            $database = 'tienda';
+
+            $db = new mysqli($host, $username, $password, $database);
             $db->query("SET NAMES 'utf8'"); 
             return $db;
         } 
